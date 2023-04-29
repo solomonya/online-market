409a5344f04e4153c5d554896520fc0d6cba71c8
Nurbolat <nurba.dav@gmail.com>
2023-04-29 23:02:33 +0600

    added filters to get all products route

--- src/domains/product/product.routes.js
+++ src/domains/product/product.routes.js
@@ -2,8 +2,20 @@ export async function productRoutes(fastify, options) {
   const { productModel, queryBuilder } = options;
 
   fastify.get("/", async (request, reply) => {
-    const products = await queryBuilder.from("get_products_with_providers").execute();
+    const { min_price, max_price, provider_name, product_name, order, order_dir } = request.query;
 
+    await queryBuilder.from("get_products_with_providers");
+    if (min_price !== undefined) queryBuilder.where(`price >= ${min_price}`);
+    if (max_price !== undefined) queryBuilder.where(`price <= ${max_price}`);
+    if (provider_name !== undefined) queryBuilder.where(`provider_name like '%${provider_name}%'`);
+    if (product_name !== undefined) queryBuilder.where(`name like '%${product_name}%'`);
+
+    if (order !== undefined) queryBuilder.orderBy(order, order_dir);
+
+    console.log("wheres", queryBuilder.whereConditions);
+    console.log("orders", queryBuilder.orderByFields);
+    const products = await queryBuilder.execute();
+    queryBuilder.reset();
     reply.status(200).send(products);
   });
 
